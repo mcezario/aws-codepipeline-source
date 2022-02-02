60033317af18844c805f9e15d52bd5f0d3d82ee7
Maikon <maikon.cezario@gmail.com>
2022-02-01 22:29:09 -0800

    add INTEGRATION_HOST to vars.tf

--- vars.tf
+++ vars.tf
@@ -3,6 +3,7 @@ variable AWS_SECRET_KEY {}
 variable AWS_REGION {
     default = "us-west-2"
 }
+variable INTEGRATION_HOST {}
 
 variable MAPPING {
     type = list(object({key=string, model=string, vtl=string}))
